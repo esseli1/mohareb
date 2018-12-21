007be309810c5d0aaaa4c74a2792f2c0d28916a3
esseli1 <46043777+esseli1@users.noreply.github.com>
2018-12-21 14:15:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : CF Na World Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Music`,"https://www.twitch.tv/esseli00")
+client.user.setGame(`1play`,"https://www.twitch.tv/esseli00")
 client.user.setStatus("dnd")
 });
 
